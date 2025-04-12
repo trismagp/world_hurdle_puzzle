58991e5884083f61d2213085bdcbc6103a6ab886
Tris <tris.magp@gmail.com>
2025-04-12 18:27:43 +0200

    Add keyboard

---
+++ lib/keyboard_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+const keysList = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+];
+
+class KeyboardView extends StatelessWidget {
+  const KeyboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          children: [
+            for (int i = 0; i < keysList.length; i++)
+              Row(
+                children:
+                    keysList[i]
+                        .map(
+                          (e) => VirtualKey(
+                            letter: e,
+                            excluded: false,
+                            onPress: (value) {},
+                          ),
+                        )
+                        .toList(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VirtualKey extends StatelessWidget {
+  final String letter;
+  final bool excluded;
+  final Function(String) onPress;
+
+  const VirtualKey({
+    super.key,
+    required this.letter,
+    required this.excluded,
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: excluded ? Colors.red : Colors.black,
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.zero,
+        ),
+        onPressed: () {
+          onPress(letter);
+        },
+        child: Text(letter),
+      ),
+    );
+  }
+}
--- lib/word_hurld_page.dart
+++ lib/word_hurld_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:world_hurdle_puzzle/hurdle_provider.dart';
+import 'package:world_hurdle_puzzle/keyboard_view.dart';
 import 'package:world_hurdle_puzzle/wordle_view.dart';
 
 class WordHurldPage extends StatefulWidget {
@@ -25,23 +26,27 @@ class _WordHurldPageState extends State<WordHurldPage> {
         child: Column(
           children: [
             Expanded(
-              child: Consumer<HurdleProvider>(
-                builder:
-                    (context, provider, child) => GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 5,
-                            mainAxisSpacing: 4,
-                            crossAxisSpacing: 4,
-                          ),
-                      itemCount: provider.hurdleBoard.length,
-                      itemBuilder: (context, index) {
-                        final wordle = provider.hurdleBoard[index];
-                        return WordleView(wordle: wordle);
-                      },
-                    ),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Consumer<HurdleProvider>(
+                  builder:
+                      (context, provider, child) => GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 5,
+                              mainAxisSpacing: 4,
+                              crossAxisSpacing: 4,
+                            ),
+                        itemCount: provider.hurdleBoard.length,
+                        itemBuilder: (context, index) {
+                          final wordle = provider.hurdleBoard[index];
+                          return WordleView(wordle: wordle);
+                        },
+                      ),
+                ),
               ),
             ),
+            KeyboardView(),
           ],
         ),
       ),
